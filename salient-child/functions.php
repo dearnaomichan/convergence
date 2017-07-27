329123b8ef27045e4cb2fa5b9b73b1c0f03b4ee9
Naomi <dearnaomichan@gmail.com>
2017-07-26 22:33:16 -0500

    gather theme files

---
+++ salient-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles');
+function salient_child_enqueue_styles() {
+
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'));
+
+    if ( is_rtl() )
+   		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
+}
+
+// function salient_child_scripts(){
+// 	wp_enqueue_script('extra js', get_stylesheet_directory_uri() . '/js/extra.js');
+// }
+// add_action('wp_enqueue_scripts', 'salient_child_scripts');
+
+
+?>
